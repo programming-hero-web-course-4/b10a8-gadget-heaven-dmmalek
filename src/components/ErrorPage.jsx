c343aaef637d81bbf85508281759dafb2c016ffd
Abdul Malek <abdulmalekmonir97@gmail.com>
2024-11-21 00:43:13 +0600

    add product image link using imgBB

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
-      <h1>404 Not Found</h1>
+    <div className="grid items-center justify-center min-h-screen">
+      <div>
+        <img src="../../public/404page.png" alt="404 Page" />
+        <div className="text-center">
+          <Link
+            to={"/"}
+            className="btn bg-customPurple rounded-3xl text-white text-xl"
+          >
+            Go to Homepage
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
